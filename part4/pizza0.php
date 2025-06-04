b398f8a0bff79717e48fc8c4c4a136df12d15f3f
nb23506-FahrurAslamiSujiwo-Faru <167288305+nb23506-FahrurAslamiSujiwo-Faru@users.noreply.github.com>
2025-06-05 00:43:59 +0900

    Add files via upload

---
+++ part4/pizza0.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html >
+<html>
+<head>
+  <meta charset="utf-8" ><!DOCTYPE html >
+<html>
+<head>
+  <meta charset="utf-8" >
+  <title>Thank you for your order</title>
+  <style type="text/css">
+  ol, ul {
+     list-style-type: none;
+      }
+  body {
+    background-color: #faf2e4;
+    margin: 0 10%;
+    font-family: sans-serif;
+  }
+  h1 {
+    text-align: center;
+    font-family: serif;
+    font-weight: normal;
+    text-transform: uppercase;
+    border-bottom: 1px solid #57b1dc;
+    margin-top: 30px;
+  }
+
+  h2 {
+    color: #d1633c;
+    font-size: 1em;
+  }
+  p.disclaimer {
+    border-top: 1px solid #d1633c;
+    padding-top: 1em;
+  }
+  </style>
+</head>
+
+<body>
+  <?php 
+  //Fill in necessary PHP code here
+  ?>
+<h1>THANK YOU</h1>
+
+<p>Thank you for ordering from Black Goose Bistro. We have received the following information about your order:</p>
+
+<h2>Your Information</h2>
+<ul>
+<li><strong>Name:</strong> <?php //Fill in necessary PHP code here ?></li>
+<li><strong>Address:</strong> <?php //Fill in necessary PHP code here ?></li>
+<li><strong>Telephone number:</strong> <?php //Fill in necessary PHP code here ?></li>
+<li><strong>Email Address:</strong> <?php //Fill in necessary PHP code here ?></li>
+</ul>
+<p><strong>Delivery instructions:</strong> <?php //Fill in necessary PHP code here ?></p>
+
+<h2>Your pizza</h2>
+
+<?php //Fill in necessary PHP code here ?>
+<em>Sorry, we did not receive your information. Please try to order again.</a></em>
+<?php//Fill in necessary PHP code here ?>
+
+<p class="disclaimer"><small>This site is for educational purposes only. No pizzas will show up at your door.</small></p>
+
+</body>
+</html>
